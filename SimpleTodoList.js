c4603fe27ec29c84109df3937a06507f93c3d77c
Kishore Narendran <kishore.narendran09@gmail.com>
2015-08-14 12:20:00 +0700

    Reverted

--- SimpleTodoList.js
+++ SimpleTodoList.js
@@ -1,10 +1,18 @@
 if (Meteor.isClient) {
-  Template.body.helpers({
-    tasks: [
-      {text: "This is task 1!"},
-      {text: "This is task 2!"},
-      {text: "This is task 3!"}
-    ]
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
   });
 }
 
